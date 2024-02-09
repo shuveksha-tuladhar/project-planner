744cfe9621abbdc02894a78cd00facdc8fb52f6a
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2024-02-09 12:09:51 -0800

    Sanitized, validated and Transform Sign-Up DTO on the backend

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import * as sanitizeHtml from 'sanitize-html';
+import { Transform } from 'class-transformer';
 
-type SignUpDto = {
+export class SignUpDto {
+    @IsNotEmpty()
+    @Transform((params) => sanitizeHtml(params.value))
     name: string;
+
+    @IsEmail()
+    @Transform((params) => sanitizeHtml(params.value))
     email: string;
+
+    @IsNotEmpty()
+    @Transform((params) => sanitizeHtml(params.value))
     username: string;
+
+    @IsNotEmpty()
     password: string;
+
 }
 
 @Controller('auth')
