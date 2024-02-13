e046a4a1d84cae414a37448619603c3d54411254
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2024-02-12 23:47:17 -0800

    Added password hashing using bcrypt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from "bcrypt";
+
 
 @Injectable()
 export class AuthService {
 constructor(private userService: UsersService){}
 
-    signUp(signUpDto) {
-        console.log('SIGN UP DTO', signUpDto)
+    async hashPassword(password: string){
+        const saltRounds = 10; 
+        return await bcrypt.hash(password, saltRounds)
+    }
 
+    async signUp(signUpDto) {
+        // check if username already exists
+
+        //check if email already exists
+
+        //hash password
+        const hashedPassword = await this.hashPassword(signUpDto.password);
+        console.log('HASHED PASSWORD:', hashedPassword);
+        signUpDto.password = hashedPassword;
+        
         // add user to User table
+        this.userService.createUser(signUpDto);
         return 'fake token';       
     }
 }
