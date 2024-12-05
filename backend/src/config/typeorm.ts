f91ce52108760c4affe66482c0b73f75a7610c90
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2024-12-04 16:49:22 -0800

    DB configuration for ssl certificate for deployment fix for ssl

--- backend/src/config/typeorm.ts
+++ backend/src/config/typeorm.ts
@@ -11,13 +11,14 @@ const config = {
   username: `${process.env.DB_USERNAME}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
-  ssl: `${process.env.DB_SSL}` && {
-    key: `${process.env.DB_SSL_KEY ?? undefined}`,
-    cert: `${process.env.DB_SSL_CERT ?? undefined}`,
-    ca: `${process.env.DB_SSL_CA ?? undefined}`,
-    capath: `${process.env.DB_SSL_CAPATH ?? undefined}`,
-    cipher: `${process.env.DB_SSL_CIPHER ?? undefined}`,
-    rejectUnauthorized: `${process.env.DB_SSL_CIPHER ?? false}`,
+  ssl: process.env.DB_SSL === 'true' && {
+    key: process.env.DB_SSL_KEY ?? undefined,
+    cert: process.env.DB_SSL_CERT ?? undefined,
+    ca: process.env.DB_SSL_CA ?? undefined,
+    capath: process.env.DB_SSL_CAPATH ?? undefined,
+    cipher: process.env.DB_SSL_CIPHER ?? undefined,
+    rejectUnauthorized:
+      process.env.DB_REJECT_UNAUTHORIZED === 'true' ? true : false,
   },
   entities: ['dist/**/entities/**/*{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
