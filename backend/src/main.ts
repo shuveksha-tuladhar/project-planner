9145f1b331cfb1896180e1a147264e42a6400b58
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2026-01-15 18:05:24 -0800

    Fix CORS issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://www.your-frontend-domain.com'], // Replace with your frontend URLs
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // If you need to handle cookies or authorization headers
+    origin:
+      process.env.NODE_ENV === 'production' ? [process.env.FRONTEND_URL] : true,
   });
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-}));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
